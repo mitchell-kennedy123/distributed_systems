dc5aedc575f1b05cb89c0045c4a46ac0c70bba97
mitchell-kennedy123 <mitchkenn49@gmail.com>
2024-08-11 17:24:05 +0930

    assignment 1

---
+++ Assignment_1/Calculator.java
@@ -0,0 +1,6 @@
+public class Calculator {
+  public static void main(String[] args) {
+    // Write your code here
+    System.out.println("Hello, World!");
+  }
+}
---
+++ Assignment_1/main.java
@@ -0,0 +1,6 @@
+public class main {
+  public static void main(String[] args) {
+    // Write your code here
+    System.out.println("Hello, World!");
+  }
+}
